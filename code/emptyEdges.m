70f84857392c1ed90fe3752b4ec84ad46407248e
Ronen Sosnik <ronens@hit.ac.il>
2026-01-12 08:44:12 +0200

    Delete code directory

--- code/emptyEdges.m
+++
@@ -1,8 +0,0 @@
-function edges = emptyEdges()
-
-edges = table([], [], [], [], [], [], 'VariableNames',{'src', 'tgt', 'IRR', 'IRR_CIlo', 'IRR_CIhi', 'qFDR'});
-edges.srcLabel = strings(0, 1); 
-edges.tgtLabel = strings(0, 1); 
-edges.Direction = strings(0, 1);
-
-end
--- code/pct.m
+++
@@ -1,8 +0,0 @@
-function s = pct(a,b)
-
-if b <= 0
-    s = "NA";
-else
-    s = sprintf('%.1f%%', 100*a/b);
-end
-end
